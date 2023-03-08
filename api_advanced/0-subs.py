15912ab86e1b7c88d23ec6ab8c2ee133ad48dbb2
boris-ny <b.nyilindek@alustudent.com>
2023-03-08 14:33:57 +0200

    Reddit API 🦐

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -12,8 +12,8 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = \
         {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
-          AppleWebKit/537.36\
-          (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+          AppleWebKit/537.36(KHTML, like Gecko) \
+         Chrome/90.0.4430.93 Safari/537.36'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
